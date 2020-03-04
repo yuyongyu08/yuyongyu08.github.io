c52a9aa3652acf5f058acc82223bce94bf871727
yuyongyu <yuyongyu@58.com>
2020-03-04 23:58:16 +0800

    add www

---
+++ www/precache-manifest.2ee0e66dac26441b94f9b0eed46e1afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b375ebcbe25c745230f22bff19ba45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb25bb77e5ad1665f4d1",
+    "url": "/static/css/main.91b8c1f7.chunk.css"
+  },
+  {
+    "revision": "e5559bb7495e671d69e4",
+    "url": "/static/js/2.ddc34f4a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ddc34f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb25bb77e5ad1665f4d1",
+    "url": "/static/js/main.fbaae678.chunk.js"
+  },
+  {
+    "revision": "398faa8534216e9c99ec",
+    "url": "/static/js/runtime-main.df2a7e15.js"
+  }
+]);
